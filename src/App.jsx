ab0da64f180ad003b933ec6f118408df0d5071d1
toconaskevin <kevin.a.toconas@gmail.com>
2024-01-17 19:37:03 -0300

    Implement infinite scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
 import './App.css'
+import InfiniteScroll from 'react-infinite-scroll-component'
 import SearchBar from './components/SearchBar.jsx'
 import MovieCard from './components/MovieCard.jsx'
 import MovieDetail from './components/MovieDetail.jsx'
@@ -8,26 +9,52 @@ import movies from './data/movies.jsx'
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('')
+  const [movieList, setMovieList] = useState(movies.slice(0, 20))
+  const location = useLocation()
 
-  const filteredMovies = movies.filter(movie =>
-    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
-  )
+  const handleSearch = (term) => {
+    setSearchTerm(term)
+  }
+
+  useEffect(() => {
+    if (location.pathname === '/') {
+      setSearchTerm('')
+    }
+  }, [location])
+
+  const fetchMoreData = () => {
+    if (movieList.length < movies.length) {
+      setMovieList(prevMovies => [
+        ...prevMovies,
+        ...movies.slice(prevMovies.length, prevMovies.length + 10)
+      ])
+    }
+  }
 
   return (
     <>
-      <SearchBar setSearchTerm={setSearchTerm} />
-      <Switch>
-        <Route exact path="/">
-          <div className="movie-list">
-            {filteredMovies.map(movie => (
-              <MovieCard key={movie.id} movie={movie} />
-            ))}
+      <Routes>
+        <Route path="/" element={
+          <div>
+            <SearchBar onSearch={handleSearch} />
+            <InfiniteScroll
+              dataLength={movieList.length}
+              next={fetchMoreData}
+              hasMore={movieList.length < movies.length}
+              loader={<h4>Loading...</h4>}
+            >
+              <div className="movie-list">
+                {movieList.filter(movie =>
+                  movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+                ).map(movie => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+              </div>
+            </InfiniteScroll>
           </div>
-        </Route>
-        <Route path="/movie/:id">
-          <MovieDetail />
-        </Route>
-      </Switch>
+        } />
+        <Route path="/movie/:id" element={<MovieDetail movies={movies} />} />
+      </Routes>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <header>
+        <Link to={'/'}>
+          <div>
+            <h1>Peliculas</h1>
+          </div>
+        </Link>
+      </header>
+      <main>
+        <App />
+      </main>
     </Router>
   </React.StrictMode>,
 )
