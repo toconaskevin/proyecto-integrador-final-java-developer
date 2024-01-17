131762f6b6f9b53f780b790d057a6ee485ec9505
toconaskevin <kevin.a.toconas@gmail.com>
2024-01-17 20:32:27 -0300

    Fixing build paths

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/proyecto-integrador-final-java-developer/'
+  plugins: [react()]
 })
