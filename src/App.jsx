499d682cc6f5f5529c9821cdd07134d246cbad42
toconaskevin <kevin.a.toconas@gmail.com>
2024-01-17 23:20:43 -0300

    Allow searchbar search over all movies

--- src/App.jsx
+++ src/App.jsx
@@ -10,23 +10,30 @@ import movies from './data/movies.jsx'
 function App() {
   const [searchTerm, setSearchTerm] = useState('')
   const [movieList, setMovieList] = useState(movies.slice(0, 20))
-  const location = useLocation()
 
   const handleSearch = (term) => {
     setSearchTerm(term)
   }
 
   useEffect(() => {
-    if (location.pathname === '/') {
-      setSearchTerm('')
-    }
-  }, [location])
+    setMovieList(
+      movies
+        .filter(movie =>
+          movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+        )
+        .slice(0, 20)
+    )
+  }, [searchTerm])
 
   const fetchMoreData = () => {
     if (movieList.length < movies.length) {
       setMovieList(prevMovies => [
         ...prevMovies,
-        ...movies.slice(prevMovies.length, prevMovies.length + 10)
+        ...movies
+          .filter(movie =>
+            movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+          .slice(prevMovies.length, prevMovies.length + 20)
       ])
     }
   }
@@ -41,12 +48,9 @@ function App() {
               dataLength={movieList.length}
               next={fetchMoreData}
               hasMore={movieList.length < movies.length}
-              loader={<h4>Loading...</h4>}
             >
               <div className="movie-list">
-                {movieList.filter(movie =>
-                  movie.title.toLowerCase().includes(searchTerm.toLowerCase())
-                ).map(movie => (
+                {movieList.map(movie => (
                   <MovieCard key={movie.id} movie={movie} />
                 ))}
               </div>
